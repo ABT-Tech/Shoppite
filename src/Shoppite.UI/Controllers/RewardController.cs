8bc961c1e1c1040202df5dabb6b2d75c2f86aa0d
Jayshree Solanki <jayshree.solanki@ashokinfotech.in>
2023-05-16 12:41:05 +0530

    rewards

---
+++ src/Shoppite.UI/Controllers/RewardController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shoppite.UI.Controllers
+{
+    public class RewardController : Controller
+    {
+        public IActionResult Rewards()
+        {
+            return View();
+        }
+        [HttpGet]
+        public  IActionResult Claim_reward()
+        {            
+            return View();
+        }
+        public IActionResult Coin_balance()
+        {
+            return View();
+        }
+        public IActionResult Bank_Coupan()
+        {
+            return View();
+        }
+        public IActionResult Reward_store()
+        {
+            return View();
+        }
+    }
+}
