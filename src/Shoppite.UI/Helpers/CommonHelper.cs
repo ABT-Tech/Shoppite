f9cea38ffd462f996c3bcf694c907f237e76b241
SohamV001 <soham.vegda@ashokinfotech.in>
2023-08-02 19:10:06 +0530

    fix issue/bugs solve in shoppite

--- src/Shoppite.UI/Helpers/CommonHelper.cs
+++ src/Shoppite.UI/Helpers/CommonHelper.cs
@@ -57,7 +57,7 @@ namespace Shoppite.UI.Helpers
             int orgid = 1;
             var subdomain = GetSubDomain(httpContext);
             if (subdomain.Contains("localhost"))
-                orgid = 19;
+                orgid = 55;
             else
             {
                 LogError(subdomain);
